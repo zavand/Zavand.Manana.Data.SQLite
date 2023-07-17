819f3d2bdbddf647a7b4a1a864b9731344aca306
zavand <zavand@gmail.com>
2023-07-16 23:08:51 -0400

    Added project

---
+++ Zavand.Manana.Data.SQLite/SqliteStorage.cs
@@ -0,0 +1,27 @@
+﻿using System.Data.Common;
+using System.Data.SQLite;
+
+namespace Zavand.Manana.Data.SQLite;
+
+public class SqliteStorage : Storage
+{
+    public override void ChangeDatabase(string databaseName)
+    {
+        throw new NotSupportedException();
+    }
+
+    protected override DbConnection CreateConnection()
+    {
+        return new SQLiteConnection(_connectionString);
+    }
+
+    protected override DbCommand CreateCommand()
+    {
+        return new SQLiteCommand();
+    }
+
+    protected override void AddParameter(DbParameterCollection parameters, string parameterName, object value)
+    {
+        ((SQLiteParameterCollection)parameters).AddWithValue(parameterName, value);
+    }
+}
